9e1a3a741137ceff44b7b57ff4d905a56bfccea2
Mirko <mirkopalancaji@gmail.com>
2019-06-13 14:01:47 +1000

    initial commit

---
+++ project.clj
@@ -0,0 +1,16 @@
+(defproject students "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [compojure "1.6.1"]
+                 [ring/ring-defaults "0.3.2"]
+                 [hiccup "1.0.5"]
+                 [org.clojure/java.jdbc "0.7.9"]
+                 [com.h2database/h2 "1.4.199"]
+                 [ring/ring-jetty-adapter "1.7.1"]]
+  :plugins [[lein-ring "0.12.5"]]
+  :ring {:handler students.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring/ring-mock "0.3.2"]]}})
